081cd85ab9172fd583218a3b7a577bb0d87d4db7
simon-verheyen <siverheyen@gmail.com>
2021-11-21 12:09:24 +0000

    first commit majicom kiosks

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    /*entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }*/
+    resolve: {
+        fallback: {
+          util: require.resolve("util/")
+        }
+    }
+
+};
